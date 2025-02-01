db10ce30a6036dd871e6d7fa7b1adddab7559bea
JohnMata0427 <jhonmata0427@gmail.com>
2025-02-01 17:00:53 -0500

    Add metadata for the audit reservation management system in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,17 @@
+import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Sistema de Gestión de Reservas de Auditorías',
+  description:
+    'Este sistema permite gestionar las reservas de auditorías, permitiendo a los usuarios reservar una auditoría, ver las auditorías reservadas y cancelarlas.',
+  authors: [{ name: 'Jhon Mata', url: 'https://github.com/JohnMata0427' }],
+  keywords: 'auditoría, reserva, gestión de reservas, sistema de gestión',
+  publisher: 'Vercel',
+};
 
 export default function RootLayout({
   children,
